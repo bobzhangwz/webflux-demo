aef6cdb45821b121097c8415a21d6ed86939c783
bozhang <>
2021-12-19 23:45:34 +0800

    optimise value correlationId with value injection

--- src/main/kotlin/io/zhpooer/demo/CorrelationIdBadgeFilter.kt
+++ src/main/kotlin/io/zhpooer/demo/CorrelationIdBadgeFilter.kt
@@ -1,5 +1,6 @@
 package io.zhpooer.demo
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.sleuth.Tracer
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
@@ -10,17 +11,20 @@ import reactor.core.publisher.Mono
 import java.util.*
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
-class CorrelationIdBadgeFilter(private val tracer: Tracer) : WebFilter, Logging {
-  private val correlationIdKey = "x-requestid"
-
+class CorrelationIdBadgeFilter(
+  private val tracer: Tracer,
+  @Value("\${application.correlation-id-key}")
+  val correlationIdKey: String
+) : WebFilter, Logging {
   override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
-    return chain.filter(exchange).doFirst {
-      if (tracer.getBaggage(correlationIdKey)?.get().isNullOrBlank()) {
-
-        val requestId = exchange.request.headers["x-request-id"]?.firstOrNull() ?: UUID.randomUUID().toString()
-        tracer.createBaggage(correlationIdKey, requestId)
-        logger().debug("Generate correlationId automatically!")
-      }
+    if (tracer.getBaggage(correlationIdKey)?.get().isNullOrBlank()) {
+      val requestId = exchange.request.headers["x-request-id"]?.firstOrNull() ?: UUID.randomUUID().toString()
+      tracer.createBaggage(correlationIdKey, requestId)
+      logger().debug("Generate correlationId automatically!")
     }
+    tracer.getBaggage(correlationIdKey)?.get()?.let {
+      exchange.response.headers.add("X-Correlation-Id", it)
+    }
+    return chain.filter(exchange)
   }
 }
--- src/main/kotlin/io/zhpooer/demo/LoggingFilter.kt
+++ src/main/kotlin/io/zhpooer/demo/LoggingFilter.kt
@@ -1,23 +1,28 @@
 package io.zhpooer.demo
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
 
-class LoggingFilter : WebFilter, Logging {
+class LoggingFilter(
+  @Value("\${management.endpoints.web.base-path}")
+  val managementPath: String
+) : WebFilter, Logging {
   private val log = logger()
 
   override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
     val request = exchange.request
     val requestInfo = "${request.method} ${request.uri}"
+    val isManageEndpoint = request.uri.path.contains(managementPath)
 
     return chain.filter(exchange).doFirst {
-      log.info(requestInfo)
+      if(!isManageEndpoint) log.info(requestInfo)
     }.doOnError {
       log.error(requestInfo, it)
     }.doFinally {
-      log.info("${exchange.response.statusCode} $requestInfo")
+      if(!isManageEndpoint) log.info("${exchange.response.statusCode} $requestInfo")
     }
   }
 }
